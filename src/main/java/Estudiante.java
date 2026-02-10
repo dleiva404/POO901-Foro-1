823b83665b9cfa58f7e08819f05bc35fcf1bf2a8
chcalderon <charly87calderon@gmail.com>
2026-02-10 10:01:50 -0600

    Metodo ingresar y buscar

---
+++ src/main/java/Estudiante.java
@@ -0,0 +1,17 @@
+public class Estudiante {
+    private String carnet;
+    private String nombreCompleto;
+
+
+public Estudiante (String carnet, String nombreCompleto) {
+    this.carnet = carnet;
+    this.nombreCompleto = nombreCompleto;
+}
+
+@Override
+public String toString() {
+    return "Carnet: " + this.carnet + " Nombre: " + this.nombreCompleto;
+}
+}
+
+
